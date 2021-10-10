e03bcfa17287e9f76c0faa74db2838b1263f4e6e
Iasi Salomon <48367845+iasisalomon@users.noreply.github.com>
2021-10-10 17:59:37 -0300

    working on logic

--- src/store/modules/items.js
+++ src/store/modules/items.js
@@ -22,9 +22,36 @@ const mutations = {
 	ADD_ITEM(state, data) {
 		state.items.push(data);
 	},
-	SELECT_REPEATED(state, data) {
+	SELECT_REPEATED(state) {
 		const itemsCopy = [...state.items];
-		
+		const charCount = [];
+		let hasDuplicateChars = [];
+		let duplicateWords = [];
+		itemsCopy.map((item) => {
+			const obj = {};
+			for (let i in item) {
+				obj[item[i]] = obj[item[i]] || 0;
+				obj[item[i]]++;
+			}
+			charCount.push(obj);
+		});
+		hasDuplicateChars = charCount.map((letterCount) => {
+			const countOnly = Object.values(letterCount);
+			console.log('countOnly :>> ', countOnly);
+			if (countOnly.includes(2)) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+		console.log('hasDuplicateChars :>> ', hasDuplicateChars);
+		console.log('itemsCopy :>> ', itemsCopy);
+		duplicateWords = itemsCopy.map((el, index) => {
+			if (hasDuplicateChars[index]) {
+				return el;
+			}
+			console.log('duplicateWords :>> ', duplicateWords);
+		});
 	},
 };
 
